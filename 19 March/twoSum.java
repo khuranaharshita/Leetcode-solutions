f310ae5b6bc1c82f9c05c21889ebffad84528ce7
khuranaharshita <92502956+khuranaharshita@users.noreply.github.com>
2025-03-19 23:14:15 +0530

    Update twoSum.java

--- 19 March/twoSum.java
+++ 19 March/twoSum.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-public class twoSum {
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
