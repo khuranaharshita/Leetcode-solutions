59cdafa9b69e0a4ffbc77b5b4a53ad288473f73b
khuranaharshita <92502956+khuranaharshita@users.noreply.github.com>
2025-03-27 22:39:35 +0530

    Create MoveZeroes.java

---
+++ 27 March/MoveZeroes.java
@@ -0,0 +1,13 @@
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int ind = 0;        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[ind];
+                nums[ind] = nums[i];
+                nums[i] = temp;                
+                ind++;
+            }
+        }
+    }
+}
